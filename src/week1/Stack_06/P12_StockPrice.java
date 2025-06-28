8dce8c09526f368a8f0b0a4862343df7ffd71023
lyg9088 <lyg9088@gmail.com>
2025-06-29 02:02:43 +0900

    fix: 중첩된 Git 레포 제거 후 파일 구조 정리

---
+++ src/week1/Stack_06/P12_StockPrice.java
@@ -0,0 +1,27 @@
+package week1.Stack_06;
+
+public class P12_StockPrice {
+    public static void main(String[] args) {
+        /*
+        * 첫 번째, 문제를 쪼개서 분석하라
+        * 초 단위로 기록된 주식 가격이 담긴 배열 prices 가 매개변수로 주어짐
+        * 가격이 떨어지지 않은 기간은 몇 초인지를 반환하기
+        *
+        * 두 번째, 제약사항 파악하고 테스트케이스 추가하기
+        * 제약 조건
+        * prices의 각 가격은 1 이상 10,000 이하인 자연수입니다.
+        * prices의 길이는 2이상 100,000 이하입니다.
+        *
+        * 세 번째, 입력값을 분석하기
+        * prices [1,2,3,2,3]
+        * return [4,3,1,1,0]
+        *
+        * */
+    }
+}
+
+class Solution12 {
+    public static void solution() {
+
+    }
+}
